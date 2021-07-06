6e5fb9251e3c95f33f548d4132398930b954b991
nanometre380 <nanometre380@gmail.com>
2021-07-06 15:30:41 +0900

    add download.sh

---
+++ download.sh
@@ -0,0 +1,12 @@
+FILE=$1
+
+if [ $FILE == "data" ]; then
+
+    URL=https://www.dropbox.com/s/l49kz27vjdqm781/data.zip?dl=0
+    ZIP_FILE=./data/data.zip
+    mkdir -p ./data/
+    wget -N $URL -O $ZIP_FILE
+    unzip $ZIP_FILE -d ./data/
+    rm $ZIP_FILE
+
+fi
